02a289e11e6d735dd14221e9ac3b631e790bd52d
Samuel Christie <emailium@gmail.com>
2015-08-20 18:09:28 -0400

    Fix greeting example

--- src/examples/greeting.js
+++ src/examples/greeting.js
@@ -1,2 +1,29 @@
-import Lux from '../lux.js'
-import stream from 'most'
+import {Lux, stream} from 'app/lux'
+
+let core = new Lux({})
+
+function processGreeting(state, action) {
+  if (action.type == 'greet') {
+    return {state: {...state, name: action.name}}
+  }
+
+  return {state}
+}
+core.register(processGreeting)
+
+function greet(name) {
+  return {type: 'greet', name}
+}
+
+core.actions.plug(
+  stream.fromEvent('input', document.getElementById('source'))
+    .map(e => greet(e.target.value))
+)
+core.actions.observe(console.log.bind(console))
+
+core.state.observe(s => {
+  console.log(s)
+  document.getElementById('sink').innerHTML = 'Hello, ' + s.name + '!'
+})
+
+window.core = core
