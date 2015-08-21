25b676b0d4bed39aecf3f199a38d7b6ea2bb2abc
Samuel Christie <emailium@gmail.com>
2015-08-21 11:53:58 -0400

    Clean up split-test, and check missing elements

--- test/split-test.js
+++ test/split-test.js
@@ -3,7 +3,7 @@ import stream from 'most'
 import split from '../src/split'
 
 describe('split', function() {
-  it('should split a simple stream of objects into separate streams', function (done) {
+  it('should split a stream of objects by field name', function (done) {
     let s = stream.from([{a:1,b:'a'}, {a:2,b:'b'}, {a:3, b:'c'}])
 
     let {a,b} = split(s, ['a', 'b'])
@@ -21,12 +21,12 @@ describe('split', function() {
   })
 
   it('should split a stream of arrays', function(done) {
-    let s = stream.from([[1,'a'], [2,'b'], [3, 'c']])
+    let s = stream.from([[1,'a'], [2,'b'], [3], [4,'c']])
     let [num, alph] = split(s)
 
     let first = num
         .reduce((a,b)=>a+b, 0).then((result) => {
-      expect(result).to.equal(6)
+      expect(result).to.equal(10)
     })
     let second = alph
         .reduce((a,b)=>a+b, '').then((result) => {
